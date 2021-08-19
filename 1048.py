51cbcbb2a21e4cf7f469458a22b9a3586c0e17cd
Lucas Araujo de Souza <69473899+LucasAraujo9@users.noreply.github.com>
2021-08-18 21:53:27 -0300

    Mudança sugerida.
    Linha 40 estava com uma falha. Se o usuário escrevesse 400.01 o código iria quebrar.

--- 1048.py
+++ 1048.py
@@ -37,19 +37,19 @@ if(salario > 0 and salario <= 400.00):
     print("Novo salario: %.2f"%novoSalario)
     print("Reajuste ganho: %.2f"%reajuste)
     print("Em percentual: 15 %")
-elif(salario > 400.01 and salario <= 800.00):
+elif(salario > 400.00 and salario <= 800.00):
     novoSalario = salario+(salario * 0.12)
     reajuste = novoSalario - salario
     print("Novo salario: %.2f"%novoSalario)
     print("Reajuste ganho: %.2f"%reajuste)
     print("Em percentual: 12 %")
-elif(salario >= 800.01 and salario <= 1200.00):
+elif(salario > 800.00 and salario <= 1200.00):
     novoSalario = salario+(salario * 0.10)
     reajuste = novoSalario - salario
     print("Novo salario: %.2f"%novoSalario)
     print("Reajuste ganho: %.2f"%reajuste)
     print("Em percentual: 10 %")
-elif(salario >= 1200.01 and salario <= 2000.00):
+elif(salario > 1200.00 and salario <= 2000.00):
     novoSalario = salario+(salario * 0.07)
     reajuste = novoSalario - salario
     print("Novo salario: %.2f"%novoSalario)
